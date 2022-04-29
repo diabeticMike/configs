cd286046d7b140380577fde2c7b35994b2bf5a87
diabeticMike <tmymichael@gmail.com>
2022-04-29 13:16:16 +0300

    add dockerfile and docker-compose

--- main.go
+++ main.go
@@ -1,4 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func readFile(path string) string {
+	content, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	text := string(content)
+	return text
+}
+
+var json = readFile("./users.json")
+
+func users(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, json)
+}
+
 func main() {
+	http.HandleFunc("/users", users)
+	http.ListenAndServe(":8000", nil)
 }
